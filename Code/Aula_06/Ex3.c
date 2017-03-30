4983c7e762d346b6d6f446825c38808bb51ab036
DiogoCaetanoGarcia <diogogarcia@unb.br>
2017-03-30 00:21:39 -0300

    Minor update code 06 questoes 07

--- Code/Aula_06/Ex3.c
+++ Code/Aula_06/Ex3.c
@@ -21,30 +21,39 @@ int main ()
 	printf("***********************************************************************\n");
 	printf("\n");
 
+	printf("****************************************************\n");
+	printf("* Antes de criar o processo-filho, a=%d, b=%d e c=%d. *\n", a, b, c);
+	printf("****************************************************\n");
+	printf("\n");
+	
 	child_pid = fork();
-	if (child_pid != 0)
+	if (child_pid == 0)
 	{
-		printf("******************************************************************\n");
-		printf("* Este texto foi escrito no terminal pelo processo PAI (ID=%d) *\n", (int) getpid());
-		printf("* Seu filho tem o ID %d.                                       *\n", child_pid);
-		printf("* Repare que compartilhamos variaveis:                           *\n");
-		printf("*    a = %d                                                       *\n", a);
-		printf("*    b = %d                                                       *\n", b);
-		printf("*    c = %d                                                       *\n", c);
-		printf("* Cuidado com o acesso compartilhado a arquivos!!!               *\n");
-		printf("******************************************************************\n");
-		printf("\n");
-	}
-	else
-	{
+		a=7;
+		b=8;
+		c=9;
 		printf("********************************************************************\n");
 		printf("* Este texto foi escrito no terminal pelo processo FILHO (ID=%d) *\n", (int) getpid());
-		printf("* Repare que compartilhamos variaveis:                             *\n");
+		printf("* Repare que cada processo tem sua versao destas variaveis:        *\n");
 		printf("*    a = %d                                                         *\n", a);
 		printf("*    b = %d                                                         *\n", b);
 		printf("*    c = %d                                                         *\n", c);
 		printf("* Cuidado com o acesso compartilhado a arquivos!!!                 *\n");
 		printf("********************************************************************\n");
+		printf("\n");
+	}
+	else
+	{
+		sleep(1);
+		printf("******************************************************************\n");
+		printf("* Este texto foi escrito no terminal pelo processo PAI (ID=%d) *\n", (int) getpid());
+		printf("* Seu filho tem o ID %d.                                       *\n", child_pid);
+		printf("* Repare que cada processo tem sua versao destas variaveis:      *\n");
+		printf("*    a = %d                                                       *\n", a);
+		printf("*    b = %d                                                       *\n", b);
+		printf("*    c = %d                                                       *\n", c);
+		printf("* Cuidado com o acesso compartilhado a arquivos!!!               *\n");
+		printf("******************************************************************\n");
 		printf("\n");
 	}
 	
