514fb09197f5d1e455db50b6eeefcb95a3f25abc
Diogo Caetano Garcia <diogogarcia@unb.br>
2017-04-25 11:52:59 -0300

    Update Ex4.c

--- Code/Aula_14/Ex4.c
+++ Code/Aula_14/Ex4.c
@@ -1,6 +1,7 @@
  // Access from ARM Running Linux
 #include "gpio_dev_mem.h"
 
+//Compile junto com o arquivo gpio_dev_mem.c
 int main(int argc, char **argv)
 {
 	// Set up gpi pointer for direct register access
