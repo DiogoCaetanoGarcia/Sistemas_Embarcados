6cc9f421d546242cf9378fecb7ee052127e02e33
DiogoCaetanoGarcia <diogogarcia@unb.br>
2017-05-02 17:40:35 +0000

    Update GPIO IN example

---
+++ Code/Aula_15/Ex4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/poll.h>
+#include <unistd.h>
+
+int main(void)
+{
+	struct pollfd pfd;
+	char buffer;
+	int btn_press;
+	system("echo 4      > /sys/class/gpio/export");
+	system("echo rising > /sys/class/gpio/gpio4/edge");
+	system("echo in     > /sys/class/gpio/gpio4/direction");
+	pfd.fd = open("/sys/class/gpio/gpio4/value", O_RDONLY);
+	if(pfd.fd < 0)
+	{
+		puts("Erro abrindo /sys/class/gpio/gpio4/value");
+		puts("Execute este programa como root.");
+		return 1;
+	}
+	pfd.events = POLLPRI | POLLERR;
+	pfd.revents = 0;
+	puts("Pressione o botao...");
+	for(btn_press=0; btn_press<5; btn_press++)
+	{
+		lseek(pfd.fd, 0, SEEK_SET);
+		read(pfd.fd, &buffer, 1);
+		poll(&pfd, 1, -1);
+		puts("Borda de descida!");
+		usleep(200000);
+	}
+	close(pfd.fd);
+	system("echo 4 > /sys/class/gpio/unexport");
+	return 0;
+}
