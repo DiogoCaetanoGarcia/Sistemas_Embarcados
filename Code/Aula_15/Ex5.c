cfecef4ef7bf3446513986117847f07f9ff8cfda
DiogoCaetanoGarcia <diogogarcia@unb.br>
2017-05-02 18:42:12 +0000

    Update_class

--- Code/Aula_15/Ex5.c
+++ Code/Aula_15/Ex5.c
@@ -3,14 +3,16 @@
 #include <sys/poll.h>
 #include <unistd.h>
 
+#define N 100
 int main(void)
 {
 	struct pollfd pfd;
 	char buffer;
 	int btn_press;
-	system("echo 4       > /sys/class/gpio/export");
-	system("echo falling > /sys/class/gpio/gpio4/edge");
-	system("echo in      > /sys/class/gpio/gpio4/direction");
+	char vals[N]={0}, pos=0, i, s;
+	system("echo 4      > /sys/class/gpio/export");
+	system("echo rising > /sys/class/gpio/gpio4/edge");
+	system("echo in     > /sys/class/gpio/gpio4/direction");
 	pfd.fd = open("/sys/class/gpio/gpio4/value", O_RDONLY);
 	if(pfd.fd < 0)
 	{
@@ -27,6 +29,16 @@ int main(void)
 		read(pfd.fd, &buffer, 1);
 		poll(&pfd, 1, -1);
 		puts("Borda de descida!");
+		//usleep(200000);
+		pos=s=0;
+		while(s<(N/2+1))
+		{
+			lseek(pfd.fd, 0, SEEK_SET);
+			read(pfd.fd, &buffer, 1);
+			vals[pos] = buffer-'0';
+			pos=(pos+1)%N;
+			for(s=i=0;i<N;i++) s+=vals[i];
+		}
 	}
 	close(pfd.fd);
 	system("echo 4 > /sys/class/gpio/unexport");
