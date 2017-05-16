50d1551ddb3a78217d6a8c14ab4f3454927e2160
DiogoCaetanoGarcia <diogogarcia@unb.br>
2017-05-16 17:14:00 +0000

    Update_code_aula_18

--- Code/Aula_18/Ex1.c
+++ Code/Aula_18/Ex1.c
@@ -4,12 +4,6 @@
 #include <fcntl.h>
 #include <linux/ioctl.h>
 #include <linux/i2c-dev.h>
-//#include <wiringPi.h>
-//#include <wiringSerial.h>
-//#include <errno.h>
-
-// Este codigo requer a biblioteca wringPi. Compile-o usando
-//    gcc Ex2.c -lwiringPi -o Ex2.out
 
 int i2c_fd;
 void ctrl_c(int sig)
@@ -24,19 +18,7 @@ int main(void)
 	signal(SIGINT, ctrl_c);
 	i2c_fd = open("/dev/i2c-1", O_RDWR);
 	ioctl(i2c_fd, I2C_SLAVE, slave_addr);
-/*	
-	if(wiringPiSetup() == -1)
-	{
-		puts("Erro em wiringPiSetup().");
-		return -1;
-	}
-	spi_fd = wiringPiSPISetup (0, 500000);
-	if(spi_fd==-1)
-	{
-		puts("Erro abrindo a SPI. Garanta que ela nao");
-		puts("esteja sendo usada por outra aplicacao.");
-		return -1;
-	}*/
+
 	while(user_input!=0)
 	{
 		puts("Digite um numero entre 1 e 5");
