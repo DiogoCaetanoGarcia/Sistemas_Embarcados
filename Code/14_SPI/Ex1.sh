be6dd650f2c66ca7d68ba1bc0afe0e8442fb0db2
Diogo Caetano Garcia <diogogarcia@unb.br>
2018-11-09 11:05:37 -0200

    Update Ex1.sh

--- Code/14_SPI/Ex1.sh
+++ Code/14_SPI/Ex1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo
-echo Você leu Instrucoes_de_uso.txt?
+echo Você leu README.md?
 echo A SPI deverá estar habilitada para o código funcionar.
 echo Vamos escrever na interface de comunicação SPI
 echo através do arquivo /dev/spidev0.0.
@@ -11,10 +11,11 @@ echo echo -ne \"\\x01\"
 echo escreve o valor hexadecimal 0x01 na tela,
 echo sem pular a linha \(\\n\).
 echo O comando
-echo -ne "\x09"
+echo echo -ne "\x01" > /dev/spidev0.0
 echo escreve o valor hexadecimal 0x01 em /dev/spidev0.0.
 echo Se você tiver compilado e executado o código MSP430_code.c
 echo no MSP430G2553, ele deverá piscar o LED uma vez.
+echo
 echo Pressione a tecla ENTER.
 read
 echo -ne "\x01" > /dev/spidev0.0
