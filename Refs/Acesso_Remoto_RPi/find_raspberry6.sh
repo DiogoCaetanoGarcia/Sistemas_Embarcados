143b7be2c654a6c1eda37f9af0eaaff06a486c5c
Diogo Caetano Garcia <diogogarcia@unb.br>
2019-09-20 17:23:09 -0300

    Delete find_raspberry6.sh

--- Refs/Acesso_Remoto_RPi/find_raspberry6.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-IFACE=eth0
-# trigger IPv6 neighbour discovery with link-local scope multicast:
-ping6 -c2 -I $IFACE ff02::1 > /dev/null
-# print the results, filtered by MAC address vendor prefix of Raspberry Pi Foundation:
-ip -6 neigh | grep b8:27:eb
