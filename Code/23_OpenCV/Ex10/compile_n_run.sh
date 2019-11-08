7d98febb6a5fd060f74fa3d1e472e0c1fae2b4ea
Diogo <diogogarcia@unb.br>
2019-11-08 11:05:38 -0300

    +1 exemplo OpenCV

--- Code/23_OpenCV/Ex10/compile_n_run.sh
+++ Code/23_OpenCV/Ex10/compile_n_run.sh
@@ -4,12 +4,6 @@ mkdir build
 cd build
 cmake ../ > /dev/null
 make > /dev/null
-p="../../Cascades/"
-c1="haarcascade_frontalface_alt.xml"
-c2="haarcascade_eye_tree_eyeglasses.xml"
-for img in ../../Imagens/*.png
-do
-	./Faces $img $p$c1 $p$c2
-done
+./Media_linhas_colunas ../../Imagens/Olho.png
 cd ..
 rm -rf build
