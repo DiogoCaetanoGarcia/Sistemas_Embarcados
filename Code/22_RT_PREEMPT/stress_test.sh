978782a147c9b2fe1a3c3ecaa6b027083b7f8587
srcaetano@gmail.com <diogogarcia@unb.br>
2020-12-02 13:13:54 -0300

    Pequena mudança stress_test.sh

--- Code/22_RT_PREEMPT/stress_test.sh
+++ Code/22_RT_PREEMPT/stress_test.sh
@@ -17,23 +17,24 @@ function send_signal_by_name()
 	fi
 }
 
-echo "Executando '$1' por 6 segundos..."
+T=5.1
+echo "Executando '$1' por $T segundos..."
 sudo $1 &
-sleep 6
+sleep $T
 send_signal_by_name $1 SIGINT
 
-echo "Executando '$1' por 6 segundos, com"
+echo "Executando '$1' por $T segundos, com"
 echo "'cat /dev/urandom > /dev/null &' em paralelo..."
 cat /dev/urandom > /dev/null &
 sudo $1 &
-sleep 6
+sleep $T
 send_signal_by_name $1 SIGINT
 
-echo "Executando '$1' por 6 segundos, com"
+echo "Executando '$1' por $T segundos, com"
 echo "'cat /dev/urandom > /dev/null &' e './eatmem.out' em paralelo..."
 ./eatmem.out &
 sudo $1 &
-sleep 6
+sleep $T
 send_signal_by_name $1 SIGINT
 sleep 1
 send_signal_by_name cat
