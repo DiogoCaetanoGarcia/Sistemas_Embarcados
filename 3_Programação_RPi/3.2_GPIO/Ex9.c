1256796296f3c6e53199e9586ad8db266f23b8f1
DiogoCaetanoGarcia <diogogarcia@unb.br>
2021-03-17 15:58:28 -0300

    Exemplos GPIO com a biblioteca wiringPi

---
+++ 3_Programação_RPi/3.2_GPIO/Ex9.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <wiringPi.h>
+
+int main(void)
+{
+	int i = 0, pin = 7;
+	wiringPiSetup();
+	pinMode(pin, INPUT);
+	pullUpDnControl(pin, PUD_UP);
+	while(i<15)
+	{
+		printf("i = %d\n", i);
+		puts("Pressione o botao");
+		while(digitalRead(pin));
+		puts("Botao pressionado");
+		while(digitalRead(pin)==0);
+		puts("Botao solto");
+		i++;
+	}
+	return 0;
+}
