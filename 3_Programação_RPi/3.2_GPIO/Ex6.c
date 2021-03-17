35cb69047d21d33fcdd331830b8fe1f3efa4a88a
Diogo Caetano Garcia <srcaetano@gmail.com>
2021-03-17 10:36:22 -0300

    Update GPIO code

--- 3_Programação_RPi/3.2_GPIO/Ex6.c
+++ 3_Programação_RPi/3.2_GPIO/Ex6.c
@@ -4,17 +4,20 @@
 //Compile junto com o arquivo gpio_dev_mem.c
 int main(int argc, char **argv)
 {
+	int i = 0;
 	// Set up gpi pointer for direct register access
 	setup_io();
 	INP_GPIO(4);
 	GPIO_PULL = 1<<4;
-	while(1)
+	while(i<15)
 	{
+		printf("i = %d\n", i);
 		puts("Pressione o botao");
 		while(GET_GPIO(4));
 		puts("Botao pressionado");
 		while(GET_GPIO(4)==0);
 		puts("Botao solto");
+		i++;
 	}
 	return 0;
 }
