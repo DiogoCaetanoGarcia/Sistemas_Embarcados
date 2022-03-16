25c13dc235f342d18e71dd1e4823e2c715c39504
DiogoCaetanoGarcia <diogogarcia@unb.br>
2022-03-16 14:06:50 -0300

    Update rev 2021_2

---
+++ 3_Programação_RPi/3.7_Revisão/Rev_2021_2/Ex05.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <wiringPi.h>
+#include <wiringPiSPI.h>
+#include <unistd.h>
+
+int spi_fd;
+void ctrl_c(int sig)
+{
+	close(spi_fd);
+	exit(-1);
+}
+
+int main(void)
+{
+	char buffer_SPI;
+	float media;
+	int contador;
+
+	signal(SIGINT, ctrl_c);
+	wiringPiSetup();
+	spi_fd = wiringPiSPISetup (0, 500000);
+	media = 0.0;
+	contador = 0;
+	while(1)
+	{
+		usleep(10000);
+		do
+		{
+			buffer_SPI = 0x55;
+			wiringPiSPIDataRW(0, &buffer_SPI, 1);
+		} while(buffer_SPI != 0xAA);
+		usleep(100);
+		buffer_SPI = 0x01;
+		wiringPiSPIDataRW(0, &buffer_SPI, 1);
+		media += (float)buffer_SPI;
+		buffer_SPI = 0x02;
+		wiringPiSPIDataRW(0, &buffer_SPI, 1);
+		media += ((float)buffer_SPI)*256.0;
+		contador++;
+		if(contador==100)
+		{
+			media /= 100.0;
+			printf("Media(A/D) = %f\n", media);
+			media = 0.0;
+			contador = 0;
+		}
+	}
+	return 0;
+}
---
+++ 3_Programação_RPi/3.7_Revisão/Rev_2021_2/Ex06.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <wiringPi.h>
+#include <wiringPiSPI.h>
+#include <unistd.h>
+
+float media;
+int contador;
+int spi_fd;
+
+void ctrl_c(int sig)
+{
+	close(spi_fd);
+	exit(-1);
+}
+
+void conversao_AD(int sig)
+{
+	char buffer_SPI;
+	do
+	{
+		buffer_SPI = 0x55;
+		wiringPiSPIDataRW(0, &buffer_SPI, 1);
+	} while(buffer_SPI != 0xAA);
+	usleep(100);
+	buffer_SPI = 0x01;
+	wiringPiSPIDataRW(0, &buffer_SPI, 1);
+	media += (float)buffer_SPI;
+	buffer_SPI = 0x02;
+	wiringPiSPIDataRW(0, &buffer_SPI, 1);
+	media += ((float)buffer_SPI)*256.0;
+	contador++;
+	if(contador==100)
+	{
+		media /= 100.0;
+		printf("Media(A/D) = %f\n", media);
+		media = 0.0;
+		contador = 0;
+	}
+}
+
+int main(void)
+{
+	signal(SIGINT, ctrl_c);
+	signal(SIGALRM, conversao_AD);
+	wiringPiSetup();
+	spi_fd = wiringPiSPISetup (0, 500000);
+	media = 0.0;
+	contador = 0;
+	ualarm(10000, 10000);
+	while(1);
+	return 0;
+}
