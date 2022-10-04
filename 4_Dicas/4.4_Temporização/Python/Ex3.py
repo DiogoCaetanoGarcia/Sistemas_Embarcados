9859525f7338bc639c0961f60223870f7d971a49
DiogoCaetanoGarcia <diogogarcia@unb.br>
2022-10-04 18:30:20 -0300

    small update

--- 4_Dicas/4.4_Temporização/Python/Ex3.py
+++ 4_Dicas/4.4_Temporização/Python/Ex3.py
@@ -1,8 +1,8 @@
 import time
 
-N = 150000000
+N = 1500000
 time_1 = time.time()
 for i in range(N):
 	pass
 time_2 = time.time()
-print("Tempo de execução do laço 'for': %4.1f s" % (time_2-time_1))
+print("Tempo de execução do laço 'for': %f s" % (time_2-time_1))
