00de0f8e473747fe06459e9a04a9b1fb3b34a20c
Diogo Caetano Garcia <diogogarcia@unb.br>
2023-06-06 09:15:10 -0300

    vcgencmd update

--- 4_Dicas/4.1_Internet/exemplos.sh
+++ 4_Dicas/4.1_Internet/exemplos.sh
@@ -163,8 +163,7 @@ case $1 in
 		echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/><title>Temperatura do Raspberry Pi</title></head><body><div class="container"><h1>' > index.html
 		echo $(date) >> index.html
 		echo '</h1><p>' >> index.html
-		echo $(/opt/vc/bin/vcgencmd measure_temp) >> index.html
-		echo $(/usr/bin/vcgencmd measure_temp) >> index.html
+		echo $(vcgencmd measure_temp) >> index.html
 		echo '</p></div></body></html>' >> index.html
 		sudo cp index.html /var/www/html/index.html
 		show_box "Acesse o servidor em um browser"
